761ef39a0ce6b29ab9c26da9410e55ec01aaf091
Thomas Seillan <smokinglaslo@gmail.com>
2022-02-18 19:13:41 +0100

    Add menu in admin layout.

--- web/src/layouts/AdminLayout/AdminLayout.tsx
+++ web/src/layouts/AdminLayout/AdminLayout.tsx
@@ -1,9 +1,29 @@
+import { Link, routes } from '@redwoodjs/router'
+
 type AdminLayoutProps = {
   children?: React.ReactNode
 }
 
 const AdminLayout = ({ children }: AdminLayoutProps) => {
-  return <>admin layout{children}</>
+  return (
+    <>
+      <ul>
+        <li>
+          <Link to={routes.showcases()}>Showcases</Link>
+        </li>
+        <li>
+          <Link to={routes.authors()}>Authors</Link>
+        </li>
+        <li>
+          <Link to={routes.tags()}>Tags</Link>
+        </li>
+        <li>
+          <Link to={routes.medias()}>Medias</Link>
+        </li>
+      </ul>
+      <main>{children}</main>
+    </>
+  )
 }
 
 export default AdminLayout
