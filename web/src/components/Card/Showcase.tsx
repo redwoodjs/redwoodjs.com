ad48e029e6d6ed109c4a7cfb968d7075e2d32fa8
Ryan <jesselockard99@gmail.com>
2022-03-03 00:07:25 -0500

    refactored ShowcaseCard to display tagline at all times

--- web/src/components/Card/Showcase.tsx
+++ web/src/components/Card/Showcase.tsx
@@ -13,22 +13,18 @@ const ShowcaseCard: FC<ShowcaseCardProps> = ({ src, subtitle, title, to }) => {
     <Link
       aria-label={`${title} Showcase Link`}
       aria-details={subtitle}
-      className="cursor-pointer group relative h-fit w-full"
+      className="group border border-orange-200 focus:border-orange-400 hover:border-orange-400 cursor-pointer h-fit w-full no-underline rounded-md"
       to={to}
     >
-      <span className="absolute inset-0 flex items-center justify-center bg-teal-900 bg-opacity-95 opacity-0  group-focus:opacity-100 group-hover:opacity-100 backdrop-blur-none group-focus:backdrop-blur-sm group-hover:backdrop-blur-sm rounded-md transition-all duration-[400ms]">
-        <div className="space-y-3 w-4/6">
-          <h3 className="font-semibold text-xl text-white tracking-wide underline underline-offset-2 decoration-orange-400">
-            {title}
-          </h3>
-          <p className="text-sm text-stone-200">{subtitle}</p>
-        </div>
-      </span>
       <img
         alt={`${title} branding`}
-        className="aspect-auto rounded-md w-full"
+        className="aspect-auto rounded-t-md w-full"
         src={src}
       />
+      <span className="bg-white border-t border-orange-200 group-focus:border-orange-400 group-hover:border-orange-400 flex flex-col p-8 pt-6 rounded-b-md space-y-2">
+        <h3 className="title">{title}</h3>
+        <p className="leading-relaxed text-sm text-stone-900">{subtitle}</p>
+      </span>
     </Link>
   )
 }
