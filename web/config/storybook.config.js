f9222051710a5f5b22195466fe270c2e21ce0caa
Kris Coulson <kriscoulson@gmail.com>
2022-03-06 09:13:59 -0800

    turn off typescript checking on startup

--- web/config/storybook.config.js
+++ web/config/storybook.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   addons: ['@storybook/addon-viewport'],
+  typescript: {
+    check: false,
+    // By default, the checker runs asynchronously in dev mode. Force it to run synchronously.
+    checkOptions: { async: false },
+  },
   previewHead: (head) => `
   ${head}
   <link rel="preconnect" href="https://fonts.googleapis.com" />
