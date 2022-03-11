85d9764a98dd7b319a4878e10db896fc5506c23c
Thomas Seillan <smokinglaslo@gmail.com>
2022-03-11 08:22:42 +0100

    Fix seeding bug.

--- scripts/seed/tag.ts
+++ scripts/seed/tag.ts
@@ -1,24 +1,27 @@
-import {db} from "api/src/lib/db"
+import { db } from 'api/src/lib/db'
 
 export default async function Tag() {
   const tags = [
-    {label: 'canon'},
-    {label: 'cms'},
-    {label: 'community'},
-    {label: 'e-commerce'},
-    {label: 'highlight'},
-    {label: 'integration'},
-    {label: 'sample'},
-    {label: 'startup'},
-    {label: 'tool'},
+    { label: 'canon' },
+    { label: 'cms' },
+    { label: 'community' },
+    { label: 'e-commerce' },
+    { label: 'highlight' },
+    { label: 'integration' },
+    { label: 'sample' },
+    { label: 'startup' },
+    { label: 'tool' },
   ]
 
-  await Promise.allSettled(tags.map(tag => db.tag.upsert({where: {label: tag.label}, create: tag, update: tag})))
+  await Promise.allSettled(
+    tags.map((tag) =>
+      db.tag.upsert({ where: { label: tag.label }, create: tag, update: tag })
+    )
+  )
 
   return await db.tag
-    .findMany({select: {id: true, label: true}})
+    .findMany({ select: { id: true, label: true } })
     .then((tags) =>
-      Object.fromEntries(tags.map(({id, label}) => [label, {id}]))
+      Object.fromEntries(tags.map(({ id, label }) => [label, { id }]))
     )
-
 }
