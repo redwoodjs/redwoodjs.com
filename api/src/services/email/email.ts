db77b9f72784d73053c99ae6dfca559df8e0f9f6
Rob Cameron <cannikin@fastmail.com>
2022-03-21 16:17:33 -0700

    Adds email service

---
+++ api/src/services/email/email.ts
@@ -0,0 +1,23 @@
+import * as nodemailer from 'nodemailer'
+
+export const mailer = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: parseInt(process.env.EMAIL_PORT),
+  secure: process.env.EMAIL_PORT === '465',
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+})
+
+export const newJob = async ({ job }) => {
+  await mailer.sendMail({
+    from: '"RedwoodJS Jobs" <jobs@redwoodjs.com>',
+    to: job.email,
+    subject: 'Your RedwoodJS job post is live!',
+    text: `Your job post is live here: https://redwoodjs.com/jobs/${job.id}\n\nIf you need to edit your post you can do that here: https://redwoodjs.com/jobs/${job.id}/edit?token=${job.token}`,
+    html: `<p>Your job post is live here: <a href="https://redwoodjs.com/jobs/${job.id}">https://redwoodjs.com/jobs/${job.id}</a></p><p>If you need to edit your job post you can do so here: <a href="https://redwoodjs.com/jobs/${job.id}/edit?token=${job.token}">https://redwoodjs.com/jobs/${job.id}/edit?token=${job.token}</a></p>`,
+  })
+}
+
+export const profileEdit = ({ jobProfile }) => {}
