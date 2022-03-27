d0b86e5fd87ff1f86b6217cb170c01e6213c363e
Kris Coulson <kriscoulson@gmail.com>
2022-03-26 19:27:47 -0700

    Add mission section

---
+++ web/src/components/MissionSection/MissionSection.tsx
@@ -0,0 +1,13 @@
+const MissionSection = () => {
+  return (
+    <section className="text-center">
+      <p className="font-serif text-xl sm:text-2xl md:text-4xl xl:text-6xl">
+        Our mission is to help <span className="font-bold">more startups</span>
+        <br /> explore{' '}
+        <span className="font-bold">more territory, more quickly.</span>
+      </p>
+    </section>
+  )
+}
+
+export default MissionSection
