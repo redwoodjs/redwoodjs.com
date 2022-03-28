c4f25887a53638d9138cad7d201d60f263bc17d0
Kris Coulson <kriscoulson@gmail.com>
2022-03-27 18:34:34 -0700

    Add testimonial section

---
+++ web/src/components/TestimonialSection/TestimonialSection.tsx
@@ -0,0 +1,66 @@
+const TestimonialSection = () => {
+  const testimonials = [
+    {
+      name: 'Ryan Chenkie',
+      subtext: 'Founder, CourseLift',
+      quote:
+        'Redwood has been a game changer for me. I use it for CourseLift and its the standard for new projects for my agency.”',
+      image: '/images/RyanChenkie.jpg',
+    },
+    {
+      name: 'Kris Coulson',
+      subtext: 'Founder, Teamstream',
+      quote:
+        'Not just ‘made with RedwoodJS’. Made with a lot of love for RedwoodJS. 💕',
+      image: '/images/KrisCoulson.jpg',
+    },
+  ]
+  return (
+    <div style={{ marginTop: 0 }}>
+      <section className="bg-blue-200 py-24 px-4">
+        <ul className="mx-auto grid gap-3 sm:grid-cols-2 lg:max-w-6xl">
+          {testimonials.map((testimonial) => (
+            <li
+              key={testimonial.name}
+              className="col-span-1 space-y-4 rounded-xl bg-white p-4"
+            >
+              <div className="flex lg:-mt-16 lg:flex-col lg:items-center lg:justify-center">
+                <img
+                  src={testimonial.image}
+                  alt={testimonial.name}
+                  className="mr-4 h-12 w-12 rounded-full lg:h-28 lg:w-28"
+                />
+                <div>
+                  <div className="text-lg font-bold">{testimonial.name}</div>
+                  <div className="text-xs font-light text-gray-600">
+                    {testimonial.subtext}
+                  </div>
+                </div>
+              </div>
+
+              <div className="relative font-serif">
+                <span className="absolute text-6xl font-bold text-gray-400">
+                  “
+                </span>
+                <blockquote className="pt-8 text-xl">
+                  {testimonial.quote}
+                </blockquote>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section className="flex flex-col items-center justify-center space-y-8 bg-black py-24">
+        <h3 className="text-center font-serif text-xl text-white md:text-4xl">
+          Ready to start your own <br />
+          Redwood adventure?
+        </h3>
+        <a href="/docs/tutorial" className="button bg-[#93CA48] text-stone-900">
+          Start the tutorial
+        </a>
+      </section>
+    </div>
+  )
+}
+
+export default TestimonialSection
