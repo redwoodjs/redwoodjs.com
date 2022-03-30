3a8bd9478d256a85d8d47a9b3a1d367d77f96663
Thomas Seillan <smokinglaslo@gmail.com>
2022-03-30 11:36:21 +0200

    Change directive translate to clean some errors and remove a console.log.

--- api/src/directives/translate/translate.ts
+++ api/src/directives/translate/translate.ts
@@ -1,10 +1,13 @@
-import {createTransformerDirective, TransformerDirectiveFunc,} from '@redwoodjs/graphql-server';
+import {
+  createTransformerDirective,
+  TransformerDirectiveFunc,
+} from '@redwoodjs/graphql-server'
 
 const AcceptLanguageParser = require('accept-language-parser')
 
 enum LanguageISOCode {
-  fr = "fra",
-  en = "eng"
+  fr = 'fra',
+  en = 'eng',
 }
 
 const DEFAULT_LANGUAGE = LanguageISOCode.en
@@ -17,36 +20,48 @@ export const schema = gql`
 `
 
 function applyTransformation(node, iso_code) {
-  if ("localizations" in node) {
-    const {localizations, ...data} = node
+  if ('localizations' in node) {
+    const { localizations, ...data } = node
 
-    const localization = localizations.filter(loc => loc.language === iso_code)?.pop() ?? false
+    const localization =
+      localizations.filter((loc) => loc.language === iso_code)?.pop() ?? false
 
     if (localization) {
       const {
-        id, isValid, language, ...translation
+        id: _id,
+        isValid: _isValid,
+        language: _language,
+        ...translation
       } = localization
 
-      return {...data, ...translation}
+      return { ...data, ...translation }
     }
 
     return data
-
   } else {
     return node
   }
 }
 
-const transform: TransformerDirectiveFunc = ({context, directiveArgs, resolvedValue}) => {
-  console.log(context)
-  // @ts-ignore
-  const requestLanguage = context.variables?.language ?? AcceptLanguageParser.parse(context.event.headers['accept-language'])?.shift()?.code
+const transform: TransformerDirectiveFunc = ({
+  context,
+  directiveArgs: _directiveArgs,
+  resolvedValue,
+}) => {
+  const requestLanguage =
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    context.variables?.language ??
+    AcceptLanguageParser.parse(
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      context.event.headers['accept-language']
+    )?.shift()?.code
 
-  // @ts-ignore
   const language = LanguageISOCode[requestLanguage] ?? DEFAULT_LANGUAGE
 
   if (resolvedValue instanceof Array) {
-    return resolvedValue.map(node => applyTransformation(node, language))
+    return resolvedValue.map((node) => applyTransformation(node, language))
   } else {
     return applyTransformation(resolvedValue, language)
   }
