80fb24770d7e950ceca839dd4fd01593e2ec023f
Thomas Seillan <smokinglaslo@gmail.com>
2022-03-31 11:08:22 +0200

    Add module classnames to web. Change Examples page for some improvements (WIP).

--- web/src/pages/ExamplesPage/components/TagsCell/TagsCell.tsx
+++ web/src/pages/ExamplesPage/components/TagsCell/TagsCell.tsx
@@ -1,5 +1,5 @@
 import type { CellFailureProps, CellSuccessProps } from '@redwoodjs/web'
-import { useTranslation } from 'react-i18next'
+import { Trans } from 'react-i18next'
 import Display from 'src/components/Tag/Display'
 import useFilters from 'src/pages/ExamplesPage/components/Filters/useFilters'
 import type { FindTags } from 'types/graphql'
@@ -19,19 +19,23 @@ export const Empty = () => {
   return <></>
 }
 
-export const Failure = ({ error }: CellFailureProps) => <></>
+export const Failure = ({ error: _error }: CellFailureProps) => <></>
 
 export const Success = ({ tags }: CellSuccessProps<FindTags>) => {
-  const { t } = useTranslation()
   const filters = useFilters()
 
   return (
     <div>
-      <p className="w-full text-center text-xl">
-        {t('ExamplesPage.filters.browseBy')}
+      <p className="w-full italic text-center text-xl">
+        <Trans
+          i18nKey="ExamplesPage.filters.browseBy"
+          components={{ straight: <span className="not-italic text-3xl" /> }}
+        />
       </p>
       <div
-        className={'flex flex-row flex-wrap m-auto justify-center w-64 md:w-96 my-8'}
+        className={
+          'flex flex-row flex-wrap m-auto justify-center w-64 md:w-96 my-8 mb-24'
+        }
       >
         {tags.map((tag) => (
           <Display
