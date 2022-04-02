6f8084685e7ad0bb41f640bec7340adcad08d42f
Kris Coulson <kriscoulson@gmail.com>
2022-04-02 15:52:06 -0700

    update funding companies

--- web/src/components/FundingShoutout/FundingShoutout.tsx
+++ web/src/components/FundingShoutout/FundingShoutout.tsx
@@ -1,8 +1,25 @@
-import SnapletLogoWhite from 'web/public/images//startup-logos/snaplet_logo_white.svg'
+import SnapletLogoWhite from 'web/public/images/startup-logos/snaplet_logo_white.svg'
 import EverfundLogo from 'web/public/images/startup-logos/everfund_logo.svg'
-import NousLogo from 'web/public/images//startup-logos/nous_logo.svg'
+import NousLogo from 'web/public/images/startup-logos/nous_logo.svg'
+import BuildPassLogo from 'web/public/images/startup-logos/buildpass_white.svg'
+import LeftLaneLogo from 'web/public/images/startup-logos/left-lane.svg'
+import PullflowLogo from 'web/public/images/startup-logos/pullflow.svg'
+import TeamstreamLogo from 'web/public/images/startup-logos/teamstream.svg'
+import UserVitalsLogo from 'web/public/images/startup-logos/uservitals.svg'
+import CourseliftLogo from 'web/public/images/startup-logos/courselift-white.svg'
 
 const FundingShoutout = () => {
+  const companies = [
+    { name: 'Snaplet', logo: SnapletLogoWhite },
+    { name: 'Everfund', logo: EverfundLogo },
+    { name: 'Nous', logo: NousLogo },
+    { name: 'BuildPass', logo: BuildPassLogo },
+    { name: 'LeftLane', logo: LeftLaneLogo },
+    { name: 'Pullflow', logo: PullflowLogo },
+    { name: 'Teamstream', logo: TeamstreamLogo },
+    { name: 'UserVitals', logo: UserVitalsLogo },
+    { name: 'Courselift', logo: CourseliftLogo },
+  ]
   return (
     <>
       <p className="text-center font-serif text-xl md:text-4xl">
@@ -20,15 +37,14 @@ const FundingShoutout = () => {
           className="mx-auto mt-6 grid grid-cols-1 gap-0.5  overflow-hidden rounded-xl md:grid-cols-3 lg:mt-8 lg:max-w-6xl
         "
         >
-          <div className="col-span-1 flex justify-center bg-neutral-800 py-4 px-8">
-            <EverfundLogo className="w-48 fill-white" />
-          </div>
-          <div className="col-span-1 flex justify-center bg-neutral-800 py-4 px-8">
-            <SnapletLogoWhite className="w-48 fill-white" />
-          </div>
-          <div className="col-span-1 flex justify-center bg-neutral-800 py-4 px-8">
-            <NousLogo className="w-48 fill-white" />
-          </div>
+          {companies.map((company) => (
+            <div
+              key={company.name}
+              className="col-span-1 flex justify-center bg-neutral-800 py-4 px-8"
+            >
+              <company.logo className="h-12 w-48 fill-white" />
+            </div>
+          ))}
         </div>
       </section>
     </>
