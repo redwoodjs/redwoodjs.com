da733570303f1294878c2f17f6c9197020034e30
Kris Coulson <kriscoulson@gmail.com>
2022-04-02 16:02:09 -0700

    update hero section styles

--- web/src/components/HeroSection/HeroSection.tsx
+++ web/src/components/HeroSection/HeroSection.tsx
@@ -2,18 +2,18 @@ import { ChevronDownIcon } from '@heroicons/react/solid'
 const HeroSection = () => {
   return (
     <section className="relative flex min-h-[calc(100vh-64px)] bg-[url(/public/images/forest.svg)] bg-cover bg-bottom bg-no-repeat">
-      <div className="flex-1 bg-gradient-to-t from-black">
-        <div className="mb-10 flex flex-1 p-4 md:p-12 lg:p-32">
-          <div className="flex flex-1 flex-col items-center justify-center space-y-12 rounded-2xl p-8 text-center text-white lg:px-12 lg:py-24">
-            <h1 className="flex flex-col text-left font-serif text-4xl font-bold md:text-6xl lg:text-center lg:text-5xl 2xl:text-7xl">
+      <div className="flex flex-1 bg-gradient-to-t from-black">
+        <div className="mb-10 flex flex-1 items-center p-4 md:p-12 lg:p-32">
+          <div className="flex flex-1 flex-col space-y-12 rounded-2xl p-8 text-left text-white lg:items-center lg:justify-center lg:px-12 lg:py-24 lg:text-center">
+            <h1 className="flex flex-col font-serif text-4xl font-bold md:text-6xl  2xl:text-7xl">
               <span>Focus on building your startup,</span>
               <span>not fighting your framework.</span>
             </h1>
-            <h2 className="text-left text-xl md:text-3xl lg:max-w-3xl lg:text-center lg:text-2xl">
+            <h2 className="text-xl md:text-3xl lg:max-w-3xl lg:text-2xl">
               Redwood is the full-stack web framework designed to help you grow
               from side project to startup.
             </h2>
-            <a href="/docs/tutorial" className="button">
+            <a href="/docs/tutorial" className="button max-w-[200px]">
               Start the Tutorial
             </a>
           </div>
