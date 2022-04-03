58b5240f34c1535e110310e6cfcb2068a9b7f359
Kris Coulson <kriscoulson@gmail.com>
2022-04-02 23:25:21 -0700

    Make the banner a little smaller

--- web/src/layouts/MainLayout/MainLayout.tsx
+++ web/src/layouts/MainLayout/MainLayout.tsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react'
 import { Toaster } from '@redwoodjs/web/toast'
-import { XIcon, SpeakerphoneIcon } from '@heroicons/react/solid'
+import { SpeakerphoneIcon } from '@heroicons/react/solid'
 
 import Navbar from './Navigation/Navbar'
 import Footer from 'src/components/Footer'
@@ -15,12 +15,12 @@ const MainLayout = ({ children }: MainLayoutProps) => {
       <Toaster toastOptions={{ className: 'mt-16' }} />
 
       <div className="bg-rw-500">
-        <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-7xl py-2 px-1 sm:px-6 lg:px-8">
           <div className="flex flex-wrap items-center justify-between">
-            <div className="flex w-full flex-1 items-center">
-              <span className="bg-rw-800 flex rounded-lg p-2">
+            <div className="flex w-full flex-1 items-center text-sm">
+              <span className="bg-rw-800 flex rounded p-1">
                 <SpeakerphoneIcon
-                  className="h-6 w-6 text-white"
+                  className="h-4 w-4 text-white"
                   aria-hidden="true"
                 />
               </span>
@@ -36,7 +36,7 @@ const MainLayout = ({ children }: MainLayoutProps) => {
             <div className="order-3 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
               <a
                 href="https://v1launchweek.redwoodjs.com"
-                className="text-rw-500 hover:bg-rw-50 flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium no-underline shadow-sm"
+                className="text-rw-500 hover:bg-rw-50 flex items-center justify-center rounded border border-transparent bg-white px-2 py-1 text-sm font-medium no-underline shadow-sm"
               >
                 Attend Events
               </a>
