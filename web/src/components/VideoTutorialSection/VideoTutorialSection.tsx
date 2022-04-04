29d05b1750009cfa28fff263160ec9d58cc1a18d
Daniel Choudhury <dannychoudhury@gmail.com>
2022-04-04 16:28:31 +0700

    Try async loading of iframe

--- web/src/components/VideoTutorialSection/VideoTutorialSection.tsx
+++ web/src/components/VideoTutorialSection/VideoTutorialSection.tsx
@@ -1,8 +1,15 @@
+import { useLayoutEffect, useState } from 'react'
+
 const VideoTutorialSection = () => {
+  const [showPlayer, setShowPlayer] = useState(false)
+  useLayoutEffect(() => {
+    setShowPlayer(true)
+  }, [])
+
   return (
-    <section className="bg-neutral-900 py-12 md:py-32">
+    <section className="py-12 bg-neutral-900 md:py-32">
       <div className="mx-auto lg:max-w-6xl">
-        <h3 className="space-y-12 px-4 font-serif text-xl text-white sm:text-center sm:text-2xl md:text-4xl lg:text-6xl lg:leading-tight">
+        <h3 className="px-4 space-y-12 font-serif text-xl text-white sm:text-center sm:text-2xl md:text-4xl lg:text-6xl lg:leading-tight">
           <div>
             Enough chit-chat.{' '}
             <span className="font-bold">You prefer videos?</span>
@@ -12,15 +19,17 @@ const VideoTutorialSection = () => {
             comprehensive tutorial!
           </div>
         </h3>
-        <div className="relative mx-auto mt-12 px-4 md:mt-32">
-          <iframe
-            className="aspect-video w-full"
-            src="https://www.youtube.com/embed/HJOzmp8oCIQ"
-            title="RedwoodJS Video Tutorial"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+        <div className="relative px-4 mx-auto mt-12 md:mt-32">
+          {showPlayer && (
+            <iframe
+              className="w-full aspect-video"
+              src="https://www.youtube.com/embed/HJOzmp8oCIQ"
+              title="RedwoodJS Video Tutorial"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          )}
         </div>
       </div>
     </section>
