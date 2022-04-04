36cc77d3d175cbbc5e258eb8312ed7336a1672d2
Daniel Choudhury <dannychoudhury@gmail.com>
2022-04-04 15:53:01 +0700

    Use react player, to see if does lazy loading

--- web/src/components/VideoTutorialSection/VideoTutorialSection.tsx
+++ web/src/components/VideoTutorialSection/VideoTutorialSection.tsx
@@ -1,8 +1,10 @@
+import ReactPlayer from 'react-player/youtube'
+
 const VideoTutorialSection = () => {
   return (
-    <section className="bg-neutral-900 py-12 md:py-32">
+    <section className="py-12 bg-neutral-900 md:py-32">
       <div className="mx-auto lg:max-w-6xl">
-        <h3 className="space-y-12 px-4 font-serif text-xl text-white sm:text-center sm:text-2xl md:text-4xl lg:text-6xl lg:leading-tight">
+        <h3 className="px-4 space-y-12 font-serif text-xl text-white sm:text-center sm:text-2xl md:text-4xl lg:text-6xl lg:leading-tight">
           <div>
             Enough chit-chat.{' '}
             <span className="font-bold">You prefer videos?</span>
@@ -12,15 +14,16 @@ const VideoTutorialSection = () => {
             comprehensive tutorial!
           </div>
         </h3>
-        <div className="relative mx-auto mt-12 px-4 md:mt-32">
-          <iframe
-            className="aspect-video w-full"
+        <div className="relative px-6 mx-auto mt-12 h-96 lg:h-[550px] md:mt-32">
+          {/* <iframe
+            className="w-full aspect-video"
             src="https://www.youtube.com/embed/HJOzmp8oCIQ"
             title="RedwoodJS Video Tutorial"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
-          ></iframe>
+          ></iframe> */}
+          <ReactPlayer url="https://www.youtube.com/embed/HJOzmp8oCIQ"           className='react-player' width="100%" height="100%"/>
         </div>
       </div>
     </section>
