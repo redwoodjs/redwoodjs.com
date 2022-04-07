badd92e96b486df0115d27f1327a77ccc859d89c
Pawel Kowalski <pavelloz@gmail.com>
2022-04-08 00:19:37 +0200

    Use LiteYoutubeEmbed instead of iframe

--- web/src/components/VideoTutorialSection/VideoTutorialSection.tsx
+++ web/src/components/VideoTutorialSection/VideoTutorialSection.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-import videoThumbnail from './thumbnail.jpg'
-import { PlayIcon } from '@heroicons/react/solid'
+import LiteYouTubeEmbed from 'react-lite-youtube-embed'
+import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
 
 const VideoTutorialSection = () => {
-  const [showVideo, setShowVideo] = useState(false)
-
   return (
     <section className="bg-neutral-900 py-12 md:py-32">
       <div className="mx-auto lg:max-w-6xl">
@@ -19,27 +16,12 @@ const VideoTutorialSection = () => {
           </div>
         </h3>
         <div className="relative mx-auto mt-12 px-4">
-          {showVideo ? (
-            <iframe
-              className="aspect-video w-full"
-              src="https://www.youtube.com/embed/HJOzmp8oCIQ?autoplay=1"
-              title="RedwoodJS Video Tutorial"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          ) : (
-            <div className="background-green-300 flex justify-center">
-              <button className="group" onClick={() => setShowVideo(true)}>
-                <PlayIcon className="absolute top-1/2 left-1/2 w-32 -translate-x-1/2 -translate-y-1/2 transform text-rw-500 text-opacity-95 transition-transform group-hover:scale-125" />
-                <img
-                  src={videoThumbnail}
-                  alt="Play Tutorial Video"
-                  className="h-full w-auto object-cover object-center md:h-[480px] lg:h-[620px]"
-                />
-              </button>
-            </div>
-          )}
+          <LiteYouTubeEmbed
+            id="HJOzmp8oCIQ"
+            title="Play RedwoodJS Video Tutorial"
+            poster="maxresdefault"
+            noCookie
+          ></LiteYouTubeEmbed>
         </div>
       </div>
     </section>
