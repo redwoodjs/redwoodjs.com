6f9bcfd8037fcb540bcde1d751e99b7fcced79fb
Kris Coulson <kriscoulson@gmail.com>
2022-07-25 16:07:28 -0700

    use image instead of bg-image for preloading

--- web/src/components/HeroSection/HeroSection.tsx
+++ web/src/components/HeroSection/HeroSection.tsx
@@ -1,8 +1,14 @@
 import { ChevronDownIcon } from '@heroicons/react/solid'
 const HeroSection = () => {
   return (
-    <section className="relative flex min-h-[calc(100vh-64px)] bg-[url(/public/images/forest.svg)] bg-cover bg-bottom bg-no-repeat">
-      <div className="flex flex-1 bg-gradient-to-t from-black">
+    <section className="relative flex min-h-[calc(100vh-64px)] bg-cover bg-bottom bg-no-repeat">
+      <div className="flex flex-1">
+        <div className="absolute top-0 left-0 -z-10 h-full w-full bg-gradient-to-t from-black object-cover object-center" />
+        <img
+          src="/images/forest.svg"
+          alt="forest"
+          className="absolute top-0 left-0 -z-20 h-full w-full object-cover object-center"
+        />
         <div className="mb-10 flex flex-1 items-center p-4 md:p-12 lg:p-32">
           <div className="flex flex-1 flex-col space-y-12 rounded-2xl p-8 text-left text-white lg:items-center lg:justify-center lg:px-12 lg:py-24 lg:text-center">
             <h1 className="flex flex-col font-serif text-4xl font-bold md:text-6xl  2xl:text-7xl 2xl:leading-[100px]">
@@ -10,8 +16,8 @@ const HeroSection = () => {
               <span>not fighting your framework.</span>
             </h1>
             <h2 className="text-xl md:text-3xl lg:max-w-3xl lg:text-2xl lg:leading-relaxed">
-              Redwood is the open source, full-stack web framework designed to
-              help you grow from side project to startup.
+              Redwood is the full-stack web framework designed to help you grow
+              from side project to startup.
             </h2>
             <a
               href="https://redwoodjs.com/docs/tutorial"
@@ -21,7 +27,7 @@ const HeroSection = () => {
             </a>
           </div>
         </div>
-        <div className="absolute bottom-0 flex h-10 w-full items-center justify-center text-white">
+        <div className="absolute bottom-0 z-20 flex h-10 w-full items-center justify-center text-white">
           <ChevronDownIcon className="h-8 w-8" />
         </div>
       </div>
